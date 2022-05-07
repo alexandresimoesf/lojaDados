baafb195e69279cbc38a3adad74e7fbe66ea4da9
Particular <alexandresimoesf@gmail.com>
2022-05-07 18:45:17 -0300

    estoque_conf.csv

--- gerador.py
+++ gerador.py
@@ -5,8 +5,8 @@ with open('estoque_conf.csv', encoding='utf-8') as f:
     for row in reader:
         produto = row['Produto']
         rep = row['Reposicao']
-        ea = int(row['Estoque antigo']) - int(rep)
+        ea = 0 if int(row['Estoque 03/05']) == 0 else int(row['Estoque 03/05']) - int(rep)
         vendas = row['Vendas']
         en = row['Estoque novo']
 
-        print('Produto: {} \n Estoque antigo: {} \n Reposicao: {} \n Vendas: {} \n Estoque novo: {} \n\n XXXX \n\n'.format(produto, ea, rep, vendas, en))
+        print('Produto: {} \n Estoque 03/05: {} \n Reposicao: {} \n Vendas: {} \n Estoque novo: {} \n\n XXXX \n\n'.format(produto, int(row['Estoque 03/05']), rep, vendas, en))
